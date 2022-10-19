cf2288e89757365650370ef43c02adefbb28493c
marcantonio99 <marcantonio8799@gamil.com>
2022-10-19 18:01:45 +0200

    price but not Discount

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,11 @@
+
 function ticketPrice(){
     let valueKmToGo = document.getElementById('inputKmToGo').value;
+    document.getElementById('price').innerHTML = `il prezzo del tuo biglietto è : ${ valueKmToGo * 0.21 } &#8364`
+    
     let valueAge = document.getElementById('inputAge').value;
+    if( valueAge<18 ){
+        
+    }
+
 }
